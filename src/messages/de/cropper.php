ba5acbeba36cc74d3054a64df0df13a2d9da9b7c
rklvnoe <administrator@n.roteskreuz.at>
2017-09-19 16:07:22 +0200

    add german

---
+++ src/messages/de/cropper.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Image Crop Editor' => 'Bildberabeitung Editor',
+    'Browse' => 'Suchen',
+    'Crop' => 'Zuschneiden',
+    'Close' => 'Schliessen',
+    'Width' => 'Breite',
+    'Height' => 'Höhe',
+    'Either "cropWidth" and "cropHeight" properties must be specified.' => '"cropWidth" ve "cropHeight" müssen definiert werden.',
+    'Double click: toggle between "move image" or "redraw area".' => 'Double click: toggle between "move image" or "redraw area".',
+];
